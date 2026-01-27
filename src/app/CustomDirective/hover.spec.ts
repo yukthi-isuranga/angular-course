2059aa0f969c3d5775e439d186a1f7f2016ecfce
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-01-27 20:32:28 +0530

    @HostListner in Angular | Directives

---
+++ src/app/CustomDirective/hover.spec.ts
@@ -0,0 +1,8 @@
+import { Hover } from './hover';
+
+describe('Hover', () => {
+  it('should create an instance', () => {
+    const directive = new Hover();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/hooks-demo/hooks-demo.ts
+++ src/app/hooks-demo/hooks-demo.ts
@@ -3,10 +3,11 @@ import { HooksDemo2 } from '../hooks-demo2/hooks-demo2';
 import { NgIf } from '@angular/common';
 import { Highlight } from '../CustomDirective/highlight';
 import { Heighlight2 } from '../CustomDirective/heighlight2';
+import { Hover } from '../CustomDirective/hover';
 
 @Component({
   selector: 'app-hooks-demo',
-  imports: [HooksDemo2, NgIf, Highlight, Heighlight2],
+  imports: [HooksDemo2, NgIf, Highlight, Heighlight2, Hover],
   templateUrl: './hooks-demo.html',
   styleUrl: './hooks-demo.css',
 })

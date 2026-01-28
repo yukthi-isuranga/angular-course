3e303d9b3121423b84aab41001cd914ed7fd21e4
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-01-28 10:56:11 +0530

    binding derective properties

--- src/app/CustomDirective/betterhighlight.ts
+++ src/app/CustomDirective/betterhighlight.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterhighlight]',
@@ -8,17 +8,23 @@ export class Betterhighlight {
     private element: ElementRef,
     private renderer: Renderer2,
   ) {}
+  @Input() backGroundColor1?: string;
+  @Input() backGroundColor2?: string;
 
-  @HostBinding('style.backgroundColor') background: string = 'yellow';
+  @HostBinding('style.backgroundColor') background?: string = this.backGroundColor1;
   @HostBinding('style.border') border: string = 'none';
 
   @HostListener('mouseenter') mouseenter() {
-    this.background = 'pink';
+    this.background = this.backGroundColor2;
     this.border = 'red 2px solid';
   }
 
   @HostListener('mouseleave') mouseleave() {
     this.background = 'transparent';
     this.border = 'none';
+  }
+
+  ngOnInit() {
+    this.background = this.backGroundColor1;
   }
 }
